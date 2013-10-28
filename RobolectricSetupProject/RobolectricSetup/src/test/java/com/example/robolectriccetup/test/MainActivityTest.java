af2fd6ec5414d405266d024f16dca90c6c3f2a4c
Marcin Polak <mpolak87@gmail.com>
2013-10-28 21:22:27 +0100

    added Robolectric configuration using gradle and Android Studio

---
+++ RobolectricSetupProject/RobolectricSetup/src/test/java/com/example/robolectriccetup/test/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.robolectriccetup.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainActivityTest {
+
+  @Test
+  public void shouldPass() {
+      assertTrue(true);
+  }
+}
